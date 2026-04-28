9225c3f691ea6e1f90d102d000ae17576b7767fb
theguyofsomething <sorrawit2606@gmail.com>
2026-04-28 09:34:32 +0700

    Update fish images in locations object

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ const baitTypes = {
 
 const locations = {
   lake: [
-    { name: "Minnow", rarity: "Common", chance: 50, value: 50, img: "https://i.imgur.com/1.png" },
-    { name: "Salmon", rarity: "Uncommon", chance: 30, value: 120, img: "https://i.imgur.com/2.png" },
-    { name: "Tuna", rarity: "Rare", chance: 15, value: 300, img: "https://i.imgur.com/3.png" },
-    { name: "Seraphine", rarity: "Legendary", chance: 5, value: 1000, img: "https://i.imgur.com/4.png" }
+    { name: "Minnow", rarity: "Common", chance: 50, value: 50, img: "https://static.wikia.nocookie.net/fisch/images/4/40/Minnow_Bait_Render.png/revision/latest?cb=20250224011628" },
+    { name: "Salmon", rarity: "Uncommon", chance: 30, value: 120, img: "https://static.wikitide.net/fischwiki/6/69/Chinook_Salmon.png" },
+    { name: "Tuna", rarity: "Rare", chance: 15, value: 300, img: "https://static.wikitide.net/fischwiki/thumb/0/0f/Bluefin_Tuna_School.png/250px-Bluefin_Tuna_School.png"},
+    { name: "Seraphine", rarity: "Legendary", chance: 5, value: 1000, img: "https://static.wikia.nocookie.net/fisch/images/e/e1/Seraphfin.png/revision/latest?cb=20260113053800" }
   ]
 };
 
